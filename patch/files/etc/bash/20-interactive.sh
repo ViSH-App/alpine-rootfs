57480357f1130066140d43e6f3e466fda7920b43
Jesse Zhu <jesse@itjesse.com>
2026-06-13 16:20:19 +0900

    Patch v5: flush bash history after every command (iSH SIGKILL-safe)

--- patch/files/etc/bash/20-interactive.sh
+++ patch/files/etc/bash/20-interactive.sh
@@ -2,6 +2,9 @@ HISTCONTROL=ignoreboth
 HISTSIZE=1000
 HISTFILESIZE=2000
 shopt -s histappend
+# iSH sessions are SIGKILLed on app switch/close, so bash never reaches the
+# normal-exit hook that flushes history to disk. Append after every command.
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a"
 
 alias ll='ls -alh'
 alias la='ls -A'
